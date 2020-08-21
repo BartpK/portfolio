7a2057078172931e540113dcf2269889fb2b0e78
Bart Kuipers <bartkuipers88@gmail.com>
2020-08-21 10:55:10 +0200

    layout tweaks

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const navButtonClose = document.querySelector('#navclosebutton')
 const navLinks = document.querySelectorAll('.navlink')
 const navContainer = document.querySelector('#navmenucontainer')
 
+document.querySelector('#whatsappbutton').addEventListener('click', () => window.open("https://wa.me/31642286298"))
+document.querySelector('#emailbutton').addEventListener('click', () => window.open("mailto:bartkuipers88@gmail.com"))
+
+
 navLinks.forEach(link => {
     link.addEventListener('click', () => toggleNav())
 })
@@ -16,7 +20,6 @@ navButtonClose.addEventListener('click', () => toggleNav())
 
 
 const toggleNav = () => {
-    console.log('clicked')
     if (navContainer.className === 'navmenuclosed') {
         navContainer.className = 'navmenuopen'
     } else {
@@ -24,7 +27,15 @@ const toggleNav = () => {
     }
 }
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
 
 //https://wa.me/31642286298
